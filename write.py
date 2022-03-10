6d385ac8a65972b393bef6333242bf21df5a42cc
jhinglish <jhinglish@gmail.com>
2022-03-10 11:49:36 +0200

    Finished all tasks

--- write.py
+++ write.py
@@ -10,6 +10,7 @@ extension determines which of these functions is used.
 
 You'll edit this file in Part 4.
 """
+
 import csv
 import json
 
@@ -24,11 +25,16 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    fieldnames = (
-        'datetime_utc', 'distance_au', 'velocity_km_s',
-        'designation', 'name', 'diameter_km', 'potentially_hazardous'
-    )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s',
+                  'designation', 'name', 'diameter_km', 'potentially_hazardous')
+
+    with open(filename, 'w') as csv_out:
+        csvwriter = csv.writer(csv_out)
+        csvwriter.writerow(fieldnames)
+        for approach in results:
+            row = [approach.time, approach.distance, approach.velocity, approach._designation, approach.neo.name,
+                   approach.neo.diameter, approach.neo.hazardous]
+            csvwriter.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -42,4 +48,23 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    dict_list = []
+
+    for approach in results:
+        dictionary = {
+            'datetime_utc': approach.time_str,
+            'distance_au': approach.distance,
+            'velocity_km_s': approach.velocity,
+            'neo': {
+                'designation': approach._designation,
+                'name': approach.neo.name,
+                'diameter_km': approach.neo.diameter,
+                'potentially_hazardous': approach.neo.hazardous
+            }
+        }
+        dict_list.append(dictionary)
+
+    json_object = json.dumps(dict_list, indent=4)
+
+    with open(filename, 'w') as json_out:
+        json_out.write(json_object)
